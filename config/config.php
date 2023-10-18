c7dc0717c7ced1340ac06fbaac3f8ed5e4016553
Mohamad Beyranvand <itsmanadi@gmail.com>
2023-10-18 17:18:27 +0330

    Base

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+
+//نام سایت
+
+define('SITE_NAME', 'your-site-name');
+
+// مسیر ریشه
+
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+define('URL_ROOT', '/');
+
+define('URL_SUBFOLDER', ”);
+
+// پارامترهای پایگاه داده
+
+define('DB_HOST', 'your-host');
+
+define('DB_USER', 'your-username');
+
+define('DB_PASS', 'your-password');
+
+define('DB_NAME', 'your-db-name');
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+// config وارد کردن فایل
+
+require_once '../config/config.php';
